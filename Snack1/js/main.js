60eb7f3e16b9619b0dc1a3b0987e5fb1f797132e
Carmine Faella <carminefaella5@gmail.com>
2023-03-11 15:46:16 +0100

    Finito anche Snack1, fine lavoro

--- Snack1/js/main.js
+++ Snack1/js/main.js
@@ -1,3 +1,5 @@
+//Definisco l'array di oggetti
+
 const arrayBike = [
     {
         name: 'Bici da corsa rossa',
@@ -16,6 +18,39 @@ const arrayBike = [
         weight: 40
     }
 ];
+
+//Estrapolo i pesi
+
+let numberWeight = arrayBike.map(Element =>{
+
+    return Element.weight
+    
+})
+
+//Definisco il numero minore
+
+const minNumber = Math.min(...numberWeight)
+
+console.log(minNumber)
+
+//Definisco la bici più leggera
+
+arrayBike.forEach(element =>{
+
+    const {name,weight} = element;
+    console.log(`La ${name} pesa ${weight} kg`);
+
+    if (weight == minNumber){
+        console.log(`La ${name} è quella che pesa meno, con un peso di ${weight} kg`)
+    }
+})
+
+
+
+
+
+
+
 
 
 
